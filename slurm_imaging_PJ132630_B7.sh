66a962d3e69b047a1a5e42a44f4f57ac63e3f236
Patrick Kamieneski <pkamieneski@Patricks-Laptop.local>
2025-05-02 11:10:14 -0400

    imaging PJ132630 B7 hi and lo res

---
+++ slurm_imaging_PJ132630_B7.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Set SBATCH Directives
+# Lines starting with "#SBATCH", before any shell commands are
+# interpreted as command line arguments to sbatch.
+# Don't put any commands before the #SBATCH directives or they will not work.
+#
+#SBATCH --export=ALL                          # Export all environment variables to job.
+#SBATCH --mail-type=BEGIN,END,FAIL            # Send email on begin, end and fail of job.
+#SBATCH --chdir=/lustre/cv/observers/cv-7429/data/PJ132630/B7/
+#SBATCH --time=0-2:0:0                        # Expected runtime of 2 hours
+#SBATCH --mem=16G                             # Memory needed by the whole job.
+
+# casa's python requires a DISPLAY for matplot, so create a virtual X server
+xvfb-run -d casa --nogui -c /lustre/cv/observers/cv-7429/data/PJ132630/B7/image_PJ132630_B7.py
