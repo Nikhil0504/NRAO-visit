0f839550bcf1333156358ce0e96e327c22f4d4c7
Patrick Kamieneski <pkamieneski@Patricks-Laptop.local>
2025-05-02 12:32:39 -0400

    add combined run

--- image_PJ132630_B7.py
+++ image_PJ132630_B7.py
@@ -22,78 +22,154 @@ switch_arr = [1,1]
 
 
 
-for ind, vis in enumerate(vis_arr):
+# High- and low- res
 
-    if switch_arr[ind]:
+if 0:
 
-        preext = ext_arr[ind]
+    for ind, vis in enumerate(vis_arr):
 
-        # get properties from first vis
-        # field
-        msmd.open(vis[0])
-        field=str(msmd.fieldsforintent("OBSERVE_TARGET#ON_SOURCE", True)[0])
-        msmd.close()
+        if switch_arr[ind]:
 
+            preext = ext_arr[ind]
 
-        # MFS
-        imagename=field+preext+".all.mfs"
-        spws=au.getScienceSpws(vis[0], intent='OBSERVE_TARGET#ON_SOURCE', returnString=False)
-        spws=','.join(map(str, spws)) # need string later
-
-        commonpars=dict(vis=vis,
-                        selectdata=True,
-                        field=field,  # this is the source
-                        imagename=imagename,
-                        specmode="mfs",
-                        parallel=parallel,
-                        deconvolver="hogbom",
-                        interpolation="nearest",
-                        spw=spws,
-                        restoringbeam='common',
-                        weighting="briggs", #robust=robust,
-                        robust=0.5,
-                        usemask='auto-multithresh', 
-                        sidelobethreshold=2.5, 
-                        noisethreshold=5.0, 
-                        lownoisethreshold=1.5, 
-                        negativethreshold=0.0, 
-                        minbeamfrac=0.3, 
-                        growiterations=75, 
-                        dogrowprune=True, 
-                        minpercentchange=1.0, 
-                        fastnoise=True,
-                        uvtaper="",
-                        pblimit=0.2,
-                        savemodel='none')
+            # get properties from first vis
+            # field
+            msmd.open(vis[0])
+            field=str(msmd.fieldsforintent("OBSERVE_TARGET#ON_SOURCE", True)[0])
+            msmd.close()
 
 
-        # dirty run
-        if 1:
-            tclean(niter=0, imsize=[2048,2048],cell=["0.02arcsec","0.02arcsec"],**commonpars)
+            # MFS
+            imagename=field+preext+".all.mfs"
+            spws=au.getScienceSpws(vis[0], intent='OBSERVE_TARGET#ON_SOURCE', returnString=False)
+            spws=','.join(map(str, spws)) # need string later
 
-            for ext in [".psf", ".pb"]: exportfits(imagename=imagename+ext,fitsimage=imagename+ext+'.fits', overwrite=True)
-            for ext in [".image"]: exportfits(imagename=imagename+ext,fitsimage=imagename+ext+'.dirty.fits', overwrite=True)
-              
+            commonpars=dict(vis=vis,
+                            selectdata=True,
+                            field=field,  # this is the source
+                            imagename=imagename,
+                            specmode="mfs",
+                            parallel=parallel,
+                            deconvolver="hogbom",
+                            interpolation="nearest",
+                            spw=spws,
+                            restoringbeam='common',
+                            weighting="briggs", #robust=robust,
+                            robust=0.5,
+                            usemask='auto-multithresh', 
+                            sidelobethreshold=2.5, 
+                            noisethreshold=5.0, 
+                            lownoisethreshold=1.5, 
+                            negativethreshold=0.0, 
+                            minbeamfrac=0.3, 
+                            growiterations=75, 
+                            dogrowprune=True, 
+                            minpercentchange=1.0, 
+                            fastnoise=True,
+                            uvtaper="",
+                            pblimit=0.2,
+                            savemodel='none')
 
 
-                                                                                                              
-        # clean run
-        if 1:
-            ia.open(imagename+".image")
-            stat=ia.statistics(axes=[0,1],algorithm="chauvenet", zscore=3, maxiter=20)
-            ia.close()
-            rmses=stat["rms"]
-            print("rms per ch", rmses)
-            rms=np.nanmedian(rmses[rmses>0])
-            print("med rms", rms)
-            rms=np.min(rmses[rmses>0])
-            print("min rms (use)", rms)
+            # dirty run
+            if 1:
+                tclean(niter=0, imsize=[2048,2048],cell=["0.02arcsec","0.02arcsec"],**commonpars)
 
-            tclean(calcres=False, calcpsf=False, imsize=[2048,2048],cell=["0.02arcsec","0.02arcsec"], niter=50_000,threshold=str(2*rms)+'Jy',pbcor=True, **commonpars)
-            for ext in [".image", ".image.pbcor",".residual",".model"]: exportfits(imagename=imagename+ext,fitsimage=imagename+ext+'.clean.fits', overwrite=True) #[".psf", ".pb"]
-             
+                for ext in [".psf", ".pb"]: exportfits(imagename=imagename+ext,fitsimage=imagename+ext+'.fits', overwrite=True)
+                for ext in [".image"]: exportfits(imagename=imagename+ext,fitsimage=imagename+ext+'.dirty.fits', overwrite=True)
+                  
 
 
+                                                                                                                  
+            # clean run
+            if 1:
+                ia.open(imagename+".image")
+                stat=ia.statistics(axes=[0,1],algorithm="chauvenet", zscore=3, maxiter=20)
+                ia.close()
+                rmses=stat["rms"]
+                print("rms per ch", rmses)
+                rms=np.nanmedian(rmses[rmses>0])
+                print("med rms", rms)
+                rms=np.min(rmses[rmses>0])
+                print("min rms (use)", rms)
 
+                tclean(calcres=False, calcpsf=False, imsize=[2048,2048],cell=["0.02arcsec","0.02arcsec"], niter=50_000,threshold=str(2*rms)+'Jy',pbcor=True, **commonpars)
+                for ext in [".image", ".image.pbcor",".residual",".model"]: exportfits(imagename=imagename+ext,fitsimage=imagename+ext+'.clean.fits', overwrite=True) #[".psf", ".pb"]
+                 
+
+
+# COMBINED
+
+
+if 1:
+
+    preext = '.combined'
+
+    vis = vis_arr[0]
+
+    # get properties from first vis
+    # field
+    msmd.open(vis[0])
+    field=str(msmd.fieldsforintent("OBSERVE_TARGET#ON_SOURCE", True)[0])
+    msmd.close()
+
+
+    # MFS
+    imagename=field+preext+".all.mfs"
+    spws=au.getScienceSpws(vis[0], intent='OBSERVE_TARGET#ON_SOURCE', returnString=False)
+    spws=','.join(map(str, spws)) # need string later
+
+    commonpars=dict(vis=vis_arr,
+                    selectdata=True,
+                    field=field,  # this is the source
+                    imagename=imagename,
+                    specmode="mfs",
+                    parallel=parallel,
+                    deconvolver="hogbom",
+                    interpolation="nearest",
+                    spw=spws,
+                    restoringbeam='common',
+                    weighting="briggs", #robust=robust,
+                    robust=0.5,
+                    usemask='auto-multithresh', 
+                    sidelobethreshold=2.5, 
+                    noisethreshold=5.0, 
+                    lownoisethreshold=1.5, 
+                    negativethreshold=0.0, 
+                    minbeamfrac=0.3, 
+                    growiterations=75, 
+                    dogrowprune=True, 
+                    minpercentchange=1.0, 
+                    fastnoise=True,
+                    uvtaper="",
+                    pblimit=0.2,
+                    savemodel='none')
+
+
+    # dirty run
+    if 1:
+        tclean(niter=0, imsize=[2048,2048],cell=["0.02arcsec","0.02arcsec"],**commonpars)
+
+        for ext in [".psf", ".pb"]: exportfits(imagename=imagename+ext,fitsimage=imagename+ext+'.fits', overwrite=True)
+        for ext in [".image"]: exportfits(imagename=imagename+ext,fitsimage=imagename+ext+'.dirty.fits', overwrite=True)
+          
+
+
+                                                                                                          
+    # clean run
+    if 1:
+        ia.open(imagename+".image")
+        stat=ia.statistics(axes=[0,1],algorithm="chauvenet", zscore=3, maxiter=20)
+        ia.close()
+        rmses=stat["rms"]
+        print("rms per ch", rmses)
+        rms=np.nanmedian(rmses[rmses>0])
+        print("med rms", rms)
+        rms=np.min(rmses[rmses>0])
+        print("min rms (use)", rms)
+
+        tclean(calcres=False, calcpsf=False, imsize=[2048,2048],cell=["0.02arcsec","0.02arcsec"], niter=50_000,threshold=str(2*rms)+'Jy',pbcor=True, **commonpars)
+        for ext in [".image", ".image.pbcor",".residual",".model"]: exportfits(imagename=imagename+ext,fitsimage=imagename+ext+'.clean.fits', overwrite=True) #[".psf", ".pb"]
+         
 
 
